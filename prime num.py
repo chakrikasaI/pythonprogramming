e125a3c733f414a03891c3c781ad03aa8b3ba09f
chakrikasaI <42572113+chakrikasaI@users.noreply.github.com>
2019-02-20 21:08:26 +0530

    Delete prime num.py

--- prime num.py
+++
@@ -1,9 +0,0 @@
-k = int(input("Enter a number: "))  
-  
-if k > 1:  
-   for i in range(2,k):  
-       if (k % i) == 0:  
-           print("is not a prime ")  
-           break  
-   else:  
-       print("is a prime ")
